0016708c59607f3f50b6d158f26e15e292cbfbb2
Dreng0t <louischicot4@gmail.com>
2025-05-02 12:25:58 +0100

    established the basic layout of the app as we understand it now, routes to all the pages we can think of jsx files for those pages and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
 import { useState } from 'react'
 
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+
+      <Navbar/>
+
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+
+        <Route path="/ingredients" element={<Ingredients/>}/>
+        <Route path="/ingredients/:ingredeientId" element={<IngredientDetails/>}/>
+        <Route path="/createIngredient" element={<CreateIngredient/>}/>
+        <Route path="/updateIngredient" element={<UpdateIngredient/>}/>
+
+        <Route path="/recipes" element={<Recipes/>}/>
+        <Route path="/recipes/:recipeId" element={<RecipeDetails/>}/>
+        <Route path="/createRecipe" element={<CreateRecipe/>}/>
+        <Route path="/updateRecipe" element={<UpdateRecipe/>}/>
+
+        <Route path='/*' element={<NotFound />}/>
+      </Routes>
     </>
   )
 }
