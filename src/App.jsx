7dc2b226daf03c49ea1bcfd430bf8f47a3df748e
Midas van Soest <midasvsoest@gmail.com>
2025-05-06 11:37:22 +0200

    feat: Bottom navigation on mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,54 @@
-import { useState, useEffect } from 'react'
-
-import './App.css'
 import { Route, Routes } from 'react-router-dom'
-
+import { useMediaQuery, useTheme } from '@mui/material';
 
 import Home from './pages/Home';
 import About from './pages/About'
 import NotFound from './pages/NotFound'
-
 import LogIn from './pages/LogIn';
-
-import { getAuth, signOut, onAuthStateChanged } from 'firebase/auth';
-
-import { auth } from './firebase';
-
+import Navbar from './components/Navbar'
 import Ingredients from './pages/Ingredients';
 import CreateIngredient from './pages/CreateIngredient'
 import UpdateIngredient from './pages/UpdateIngredient'
-
 import Recipes from './pages/Recipes'
 import RecipeDetails from './pages/RecipeDetails'
 import CreateRecipe from './pages/CreateRecipe'
 import UpdateRecipe from './pages/UpdateRecipe'
-
 import UserRecipes from './pages/UserRecipes';
 import UserRecipeDetails from './pages/UserRecipeDetails';
+import BottomNav from './components/BottomNav'
 
-
-import Navbar from './components/Navbar'
+import './App.css'
 
 function App() {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
   return (
     <>
 
-      <Navbar/>
+      <Navbar />
 
       <Routes>
-    
-        <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/logIn" element={<LogIn/>}/>
 
-        <Route path="/ingredients" element={<Ingredients/>}/>
-        <Route path="/createIngredient" element={<CreateIngredient/>}/>
-        <Route path="/updateIngredient/:ingredientId" element={<UpdateIngredient/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/logIn" element={<LogIn />} />
 
-        <Route path="/recipes" element={<Recipes/>}/>
-        <Route path="/recipes/:recipeId" element={<RecipeDetails/>}/>
-        <Route path="/createRecipe" element={<CreateRecipe/>}/>
-        <Route path="/updateRecipe/:recipeId" element={<UpdateRecipe/>}/>
-        <Route path="/userRecipes" element={<UserRecipes/>}/>
-        <Route path="/userRecipes/:recipeId" element={<UserRecipeDetails/>}/>
+        <Route path="/ingredients" element={<Ingredients />} />
+        <Route path="/createIngredient" element={<CreateIngredient />} />
+        <Route path="/updateIngredient/:ingredientId" element={<UpdateIngredient />} />
 
-        <Route path='/*' element={<NotFound />}/>
+        <Route path="/recipes" element={<Recipes />} />
+        <Route path="/recipes/:recipeId" element={<RecipeDetails />} />
+        <Route path="/createRecipe" element={<CreateRecipe />} />
+        <Route path="/updateRecipe/:recipeId" element={<UpdateRecipe />} />
+        <Route path="/userRecipes" element={<UserRecipes />} />
+        <Route path="/userRecipes/:recipeId" element={<UserRecipeDetails />} />
+
+        <Route path='/*' element={<NotFound />} />
       </Routes>
+
+      {isMobile && <BottomNav />}
     </>
   )
 }
